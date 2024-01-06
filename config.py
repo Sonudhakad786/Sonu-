0691a6d8c0391ccc03bdb0f930c9f223e18a958f
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-01-06 07:09:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ DB_NAME = environ.get("DB_NAME", "vjbotz")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "6460619866:AAEB4vuo8FLGOCea8pq8s4KqFaUay1GU8r0")
 BOT_USERNAME = environ.get("BOT_USERNAME", "Deendayal_File_Store_bot") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/118a227a8170cafb6a4dd.jpg https://graph.org/file/118a227a8170cafb6a4dd.jpg https://graph.org/file/118a227a8170cafb6a4dd.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
