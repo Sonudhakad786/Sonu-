6098fb646bdf88b397cc1ebb513d99af08468a72
Deendayal423 <151893234+Deendayal423@users.noreply.github.com>
2024-02-21 19:52:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://deendayalfilestorebot-9bfac59397e1.herokuapp.com"
+        URL = "https://actual-junette-deendayal.koyeb.app"
     else:
-        URL = "https://deendayalfilestorebot-9bfac59397e1.herokuapp.com/"
+        URL = "https://actual-junette-deendayal.koyeb.app/"
 
 
 
